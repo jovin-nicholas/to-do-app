c5fc4adf13009041938a96ae53e97b9ec7b9f6dd
Jovin Nicholas <32580407+jovin-nicholas@users.noreply.github.com>
2024-02-02 03:33:04 -0500

    Updated task add, update, delete funtions

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,69 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const getAllTodos = async () => {
+  const res = await fetch(`http://localhost:3001/todos`, { cache: "no-store" });
+  const todos = await res.json();
+  return todos;
+};
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const addTask = async (newTask) => {
+  const task = { id: Date.now().toString(), text: newTask, status: false };
+  const response = await fetch('http://localhost:3001/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  });
+
+  try {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  console.log(response);
+
+  const addedTask = await response.json();
+  return addedTask;
+};
+
+const updateTask = async (task) => {
+  const response = await fetch(`http://localhost:3001/todos/${task.id}`, { // Add the task ID to the URL
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  });
+  try {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(response);
+
+  const updatedTask = await response.json();
+  return updatedTask;
+};
+
+const updateTaskList = async (taskList) => {
+  for (let task of taskList) {
+    await updateTask(task);
+  }
+};
+
+const deleteTask = async (id) => {
+  const response = await fetch(`http://localhost:3001/todos/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+};
+
+export { getAllTodos, updateTask, addTask, updateTaskList, deleteTask };
+
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Image from "next/image";
-import Task from "../Task";
+import Task from "./Task.jsx";
+import { useState } from "react";
+import { useEffect } from "react";
 import { Raleway } from "next/font/google";
+import { getAllTodos, updateTask, addTask, updateTaskList, deleteTask } from "./api/hello";
 
 const raleway = Raleway({
   weight: '400',
@@ -12,6 +14,61 @@ export default function Home() {
   const [newTask, setNewTask] = useState('');
   const [percentageCompleted, setPercentageCompleted] = useState(0);
 
+  // Display the tasks
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const allTodo = await getAllTodos();
+      setTasks(allTodo);
+      console.log(allTodo);
+    };
+
+    fetchTodos();
+  }, []);
+
+  // Fetch tasks from the server (connect frontend to backend)
+  const handleTaskCompletion = (id) => {
+    setTasks((prevTasks) => {
+      const updatedTasks = prevTasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, status: !task.status };
+        } else {
+          return task;
+        }
+      });
+      updateTaskList(updatedTasks);
+      return updatedTasks;
+    });
+  }
+
+  const handleAddTask = async () => {
+    if (newTask.trim() !== '') {
+      const addedTask = await addTask(newTask);
+      setTasks((prevTasks) => [...prevTasks, addedTask]);
+      setNewTask('');
+    }
+  };
+
+  const handleDeleteTask = async (id) => {
+    await deleteTask(id)
+      .then(() => console.log(`Deleted task with id: ${id}`))
+      .catch((error) => console.error(`Error: ${error}`));;
+    setTasks((prevTasks) => {
+      return prevTasks.filter((task) => task.id !== id);
+    });
+  }
+
+  const calculatePercentageCompleted = async () => {
+    const completedTasksCount = tasks.filter((task) => task.status).length;
+    const totalTasksCount = tasks.length;
+    const percentage = (totalTasksCount > 0)
+      ? ((completedTasksCount / totalTasksCount) * 100).toFixed(2)
+      : 0;
+    setPercentageCompleted(percentage);
+    for (let task of tasks) {
+      await updateTask(task);
+    }
+  }
+  
   return (
     <div className={raleway.className}>
       <div className="flex flex-row items-center justify-center mb-4">
