464aa093d3ecc0a9b3538fed6687187fcb3906c1
Jovin Nicholas <32580407+jovin-nicholas@users.noreply.github.com>
2024-02-02 01:57:55 -0500

    updated styles and task component

---
+++ pages/Task.jsx
@@ -0,0 +1,22 @@
+const Task = ({ task, onTaskCompletion, onDelete }) => {
+    const handleCheckboxChange = () => {
+      onTaskCompletion(task.id);
+    };
+  
+    return (
+      <div className='flex flex-row items-center justify-start'>
+        <input
+          type="checkbox"
+          checked={task.status}
+          onChange={handleCheckboxChange}
+          className='mr-2 text-xxl'
+        />
+        {/* if task is completed, add line-through class */}
+        <p className={`${task.status ? 'line-through' : ''}`}>{task.text}</p>
+        <button onClick={() => onDelete(task.id)}>Delete</button>
+      </div>
+    );
+  };
+  
+  export default Task;
+  
